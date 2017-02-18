1ae9ef97dd49bbce92744a4a7093e81804140d82
Axel Samuelsson <harby@spotify.com>
2017-02-18 23:55:04 +0100

    Modular approach to allow for iOS app

--- script.js
+++ script.js
@@ -1,52 +1,6 @@
-var airports = {};
-
-// sweden
-airports.arn = { name: "Stockholm Arlanda", coords: {lat: 59.6498, lng: 17.9238} };
-airports.bma = { name: "Stockholm Bromma", coords: {lat: 59.3548, lng: 17.9427} };
-airports.got = { name: "Göteborg Landvetter", coords: {lat: 57.6688, lng: 12.2923} };
-airports.lla = { name: "Luleå Kallax", coords: {lat: 65.3240, lng: 22.0741} };
-airports.mmx = { name: "Malmö Sturup", coords: {lat: 55.5355, lng: 13.3724} };
-airports.nyo = { name: "Stockholm Skavsta", coords: {lat: 58.7890, lng: 16.9154} };
-airports.ume = { name: "Umeå", coords: {lat: 63.7930, lng: 20.2893} };
-
-// usa
-airports.bos = { name: "Boston Logan", coords: {lat: 42.3656, lng: -71.0096} };
-airports.den = { name: "Denver", coords: {lat: 39.8561, lng: -104.6737} };
-airports.iad = { name: "Washington D.C. Dulles", coords: {lat: 38.9531, lng: -77.4565} };
-airports.sfo = { name: "San Francisco", coords: {lat: 37.7749, lng: -122.4194} };
-airports.mia = { name: "Miami", coords: {lat: 25.7959, lng: -80.2870} };
-airports.sea = { name: "Seattle-Tacoma", coords: {lat: 47.4502, lng: -122.3088} };
-airports.lax = { name: "Los Angeles", coords: {lat: 33.9416, lng: -118.4085} };
-airports.jfk = { name: "New York John F. Kennedy", coords: {lat: 40.6413, lng: -73.7781} };
-
-// europe
-airports.aja = { name: "Corsica Ajaccio", coords: {lat: 41.9214, lng: 8.7966} };
-airports.biq = { name: "Biarritz", coords: {lat: 43.4692, lng: -1.5303} };
-airports.cdg = { name: "Paris Charles de Gaulle", coords: {lat: 49.0097, lng: 2.5479} };
-airports.cph = { name: "Copenhagen Kastrup", coords: {lat: 55.6180, lng: 12.6508} };
-airports.dub = { name: "Dublin", coords: {lat: 53.4264, lng: -6.2499} };
-airports.fco = { name: "Rome Leonardo da Vinci–Fiumicino", coords: {lat: 41.7999, lng: 12.2462} };
-airports.hel = { name: "Helsinki", coords: {lat: 60.3210, lng: 24.9529} };
-airports.lhr = { name: "London Heathrow", coords: {lat: 51.4700, lng: -0.4543} };
-airports.lgw = { name: "London Gatwick", coords: {lat: 51.1537, lng: -0.1821} };
-airports.mad = { name: "Madrid", coords: {lat: 40.4839, lng: -3.5680} };
-airports.ory = { name: "Paris Orly", coords: {lat: 48.7262, lng: 2.3652} };
-airports.osl = { name: "Oslo Gardermoen", coords: {lat: 60.1976, lng: 11.1004} };
-airports.szg = { name: "Salzburg Wolfgang Amadeus Mozart", coords: {lat: 47.7926, lng: 13.0029} };
-airports.waw = { name: "Warsaw Chopin", coords: {lat: 52.1672, lng: 20.9679} };
-airports.zrh = { name: "Zürich", coords: {lat: 47.4582, lng: 8.5555} };
-
-// asia
-airports.hkg = { name: "Hong Kong", coords: {lat: 22.3080, lng: 113.9185} };
-airports.pek = { name: "Beijing", coords: {lat: 40.0799, lng: 116.6031} };
-
-// africa
-airports.rmf = { name: "Marsa Alam", coords: {lat: 25.5588, lng: 34.5882} };
-airports.lpa = { name: "Gran Canaria Las Palmas", coords: {lat: 27.9332, lng: -15.3877} };
-
 var map, icon_filled, icon_filled_05, icon_filled_01, flights = [], airport_markers = {};
 
-function newflight(path) {
+function newflight(path, airports) {
   var geodesicPoly = new google.maps.Polyline({
     strokeColor: '#990033',
     strokeOpacity: 1.0,
@@ -170,14 +124,6 @@ function initMap() {
     streetViewControl: false
   });
 
-  var bounds = new google.maps.LatLngBounds();
-  for (var id in airports) {
-    bounds.extend(airports[id].coords);
-    newairport(airports[id].coords, id);
-  }
-  map.fitBounds(bounds);
-  // map.setZoom(map.getZoom()-1);
-
   retro = [
             {
               elementType: 'geometry',
@@ -299,48 +245,29 @@ function initMap() {
           ];
   map.setOptions({styles: retro});
 
-  var paths = [
-                ["arn", "bos"],
-                ["arn", "hel"],
-                ["hel", "hkg"],
-                ["arn", "pek"],
-                ["pek", "hkg"],
-                ["sfo", "cph"],
-                ["cph", "arn"],
-                ["bos", "den"],
-                ["arn", "lhr"],
-                ["lhr", "iad"],
-                ["bos", "lgw"],
-                ["lgw", "arn"],
-                ["bos", "mia"],
-                ["bos", "sea"],
-                ["sea", "sfo"],
-                ["lax", "bos"],
-                ["arn", "jfk"],
-                ["nyo", "biq"],
-                ["arn", "zrh"],
-                ["zrh", "bos"],
-                ["arn", "cdg"],
-                ["cdg", "bos"],
-                ["arn", "ory"],
-                ["arn", "rmf"],
-                ["bma", "mmx"],
-                ["arn", "osl"],
-                ["arn", "got"],
-                ["arn", "ume"],
-                ["nyo", "lpa"],
-                ["arn", "fco"],
-                ["arn", "szg"],
-                ["arn", "waw"],
-                ["arn", "dub"],
-                ["nyo", "aja"],
-                ["mia", "mad"],
-                ["mad", "arn"],
-                ["cph", "lhr"],
-                ["arn", "lla"],
-                ["bos", "cph"]
-              ];
-  for (var i=0; i < paths.length; i++) {
-    newflight(paths[i]);
-  }
+  fetch('airports.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(airports) {
+    console.log(airports);
+    var bounds = new google.maps.LatLngBounds();
+    for (var id in airports) {
+      bounds.extend(airports[id].coords);
+      newairport(airports[id].coords, id);
+    }
+    map.fitBounds(bounds);
+    // map.setZoom(map.getZoom()-1);
+
+    fetch('flights.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(paths) {
+      for (var i=0; i < paths.length; i++) {
+        newflight(paths[i], airports);
+      }
+    });
+  });
+
 }
