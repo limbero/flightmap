e8b85fc53c717e945c870dadc412190e0fb6c52f
Axel Samuelsson <harby@spotify.com>
2018-02-13 16:00:35 +0100

    Clean up and make zoom easier

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
-var map, icon_filled, icon_filled_05, icon_filled_01, flights = [], airport_markers = {};
+let map, flights = [], airport_markers = {};
+const iconSizeAtZoomLevel = [null, 3, 3, 3, 3, 5, 7, 9]
 
 function newflight(path, airports) {
-  var geodesicPoly = new google.maps.Polyline({
+  let geodesicPoly = new google.maps.Polyline({
     strokeColor: '#990033',
     strokeOpacity: 1.0,
     strokeWeight: 1,
@@ -14,33 +15,33 @@ function newflight(path, airports) {
 }
 
 function newairport(location, id, airports) {
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     position: location,
     map: map,
     id: id,
-    icon: icon_filled_05
+    icon: circleIcon(0.5, iconSizeAtZoomLevel[map.getZoom()])
   });
 
   airport_markers[marker.id] = marker;
 
   google.maps.event.addListener(marker, 'mouseover', function (event) {
-    for (var id in airport_markers) {
-      airport_markers[id].setIcon(icon_filled_01);
+    for (let id in airport_markers) {
+      airport_markers[id].setIcon(circleIcon(0.1, iconSizeAtZoomLevel[map.getZoom()]));
     }
 
-    this.setIcon(icon_filled);
-    var this_airport_name = airports[this.id].name;
-    var connects = [];
+    this.setIcon(circleIcon(1.0, iconSizeAtZoomLevel[map.getZoom()]));
+    let this_airport_name = airports[this.id].name;
+    let connects = [];
 
-    for (var i=0; i < flights.length; i++) {
+    for (let i=0; i < flights.length; i++) {
       if (this.id == flights[i].path[0]) {
-        airport_markers[flights[i].path[1]].setIcon(icon_filled);
+        airport_markers[flights[i].path[1]].setIcon(circleIcon(1.0, iconSizeAtZoomLevel[map.getZoom()]));
         connects.push(airports[flights[i].path[1]].name);
         flights[i].polygon.setOptions({
                 strokeOpacity : 1.0
             });
       } else if (this.id == flights[i].path[1]) {
-        airport_markers[flights[i].path[0]].setIcon(icon_filled);
+        airport_markers[flights[i].path[0]].setIcon(circleIcon(1.0, iconSizeAtZoomLevel[map.getZoom()]));
         connects.push(airports[flights[i].path[0]].name);
         flights[i].polygon.setOptions({
                 strokeOpacity : 1.0
@@ -53,16 +54,16 @@ function newairport(location, id, airports) {
     }
     connects.sort();
 
-    var infobox = document.getElementById("infobox");
+    let infobox = document.getElementById("infobox");
     infobox.getElementsByTagName("h2")[0].innerHTML = this_airport_name;
 
-    var ul = document.createElement("ul");
-    for (var i=0; i < connects.length; i++) {
-      var li = document.createElement("li");
+    let ul = document.createElement("ul");
+    for (let i=0; i < connects.length; i++) {
+      let li = document.createElement("li");
       li.innerHTML = connects[i];
       ul.appendChild(li)
     }
-    var p = infobox.getElementsByTagName("p")[0];
+    let p = infobox.getElementsByTagName("p")[0];
     while (p.hasChildNodes()) {
       p.removeChild(p.lastChild);
     }
@@ -72,56 +73,45 @@ function newairport(location, id, airports) {
   });
 
   google.maps.event.addListener(marker, 'mouseout', function (event) {
-    for (var id in airport_markers) {
-      airport_markers[id].setIcon(icon_filled_05);
+    for (let id in airport_markers) {
+      airport_markers[id].setIcon(circleIcon(0.5, iconSizeAtZoomLevel[map.getZoom()]));
     }
 
-    for (var i=0; i < flights.length; i++) {
+    for (let i=0; i < flights.length; i++) {
       flights[i].polygon.setOptions({
               strokeOpacity : 1.0
           });
     }
 
-    var infobox = document.getElementById("infobox");
+    let infobox = document.getElementById("infobox");
     infobox.style.visibility = "hidden";
   });
 }
 
+function circleIcon(opacity, scale) {
+  return {
+    path: google.maps.SymbolPath.CIRCLE,
+    strokeColor: '#990033',
+    strokeOpacity: 0.0,
+    strokeWeight: 0,
+    fillColor: '#990033',
+    fillOpacity: opacity,
+    scale: scale //pixels
+  };
+}
+
 function initMap() {
-  icon_filled = {
-    path: google.maps.SymbolPath.CIRCLE,
-    strokeColor: '#990033',
-    strokeOpacity: 0.0,
-    strokeWeight: 0,
-    fillColor: '#990033',
-    fillOpacity: 1.0,
-    scale: 3 //pixels
-  };
-
-  icon_filled_05 = {
-    path: google.maps.SymbolPath.CIRCLE,
-    strokeColor: '#990033',
-    strokeOpacity: 0.0,
-    strokeWeight: 0,
-    fillColor: '#990033',
-    fillOpacity: 0.5,
-    scale: 3 //pixels
-  };
-
-  icon_filled_01 = {
-    path: google.maps.SymbolPath.CIRCLE,
-    strokeColor: '#990033',
-    strokeOpacity: 0.0,
-    strokeWeight: 0,
-    fillColor: '#990033',
-    fillOpacity: 0.1,
-    scale: 3 //pixels
-  };
-
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.000, lng: 0.000},
-    zoom: 4,
+    zoom: 3,
     streetViewControl: false
+  });
+
+  google.maps.event.addListener(map, 'zoom_changed', function() {
+    console.log('new zoom: ' + map.getZoom());
+    for (let id in airport_markers) {
+      airport_markers[id].setIcon(circleIcon(0.5, iconSizeAtZoomLevel[map.getZoom()]));
+    }
   });
 
   fetch('/data/theme.json').then(function(response) {
@@ -133,11 +123,10 @@ function initMap() {
   fetch('/data/airports.json')
   .then(function(response) {
     return response.json();
-  })
-  .then(function(airports) {
+  }).then(function(airports) {
     console.log(airports);
-    var bounds = new google.maps.LatLngBounds();
-    for (var id in airports) {
+    let bounds = new google.maps.LatLngBounds();
+    for (let id in airports) {
       bounds.extend(airports[id].coords);
       newairport(airports[id].coords, id, airports);
     }
@@ -147,9 +136,8 @@ function initMap() {
     fetch('/data/flights.json')
     .then(function(response) {
       return response.json();
-    })
-    .then(function(paths) {
-      for (var i=0; i < paths.length; i++) {
+    }).then(function(paths) {
+      for (let i=0; i < paths.length; i++) {
         newflight(paths[i], airports);
       }
     });
